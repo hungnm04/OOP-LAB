82d919346c75d9d705d28eb2bcacf8daa7b0391f
Hung Manh Ngo <89698425+hungnm04@users.noreply.github.com>
2024-11-25 12:51:27 +0700

    Enter the file

--- Lab02/OtherProject/src/hust/soict/dsai/lab01/_211.java
+++
@@ -1,8 +0,0 @@
-package hust.soict.dsai.lab01;
-
-public class _211 {
-    public static void main(String[] args) {
-        System.out.println("Xin chao \n cac ban");
-        System.out.println("Hello \t World!");
-    }
-}
